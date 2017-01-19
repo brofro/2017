f8be0085220f6cfa7f645b9fd5c5ae245ac421fd
brofro <jettecantiller@gmail.com>
2017-01-19 09:16:47 -0800

    removing apikey

--- Utility/DomainModel/OpenWeatherAPI/OpenWeatherApiConstants.py
+++ Utility/DomainModel/OpenWeatherAPI/OpenWeatherApiConstants.py
@@ -7,7 +7,7 @@ class OpenWeatherApiConstants(object):
 
     Name = "OpenWeatherApi"
 
-    ApiKey = "194081ece66e11a6bf8e2e9c1dda3a4d"
+    ApiKey = ""
 
     MainRoute = "http://api.openweathermap.org/data/2.5"
 
